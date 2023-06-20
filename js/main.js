2c80dc9d538b5b42808898b466efabdaaade8a1d
Dewa Jayon <dewajayon3@gmail.com>
2023-06-20 08:57:41 +0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(".gallery a").on("open.simplelightbox", function () {
+    // do something…
+});
+
+// As A Vanilla JavaScript Plugin
+var gallery = new SimpleLightbox(".gallery a", {
+    /* options */
+});
